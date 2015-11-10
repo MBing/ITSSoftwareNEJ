142ba462ac837f6defa51566f9e61dfbf17c10ca
Martin Bing <bing.martin.photography@gmail.com>
2015-11-10 12:48:45 +0100

    Update modules logger change add browser sync

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 var express = require('express'),
-    logger = require('express-log'),
+    morgan = require('morgan'),
     stylus = require('stylus'),
-    nib = require('nib');
+    nib = require('nib'),
+    bs = require('browser-sync').create();
 
 
 var app = express();
@@ -15,7 +16,7 @@ function compile(str, path) {
 
 app.set('views', __dirname + '/views');
 app.set('view engine', 'jade');
-app.use(logger());
+app.use(morgan(':id :method :url :response-time'))
 app.use(stylus.middleware(
     {   src: __dirname + '/public',
         compile: compile
@@ -29,5 +30,13 @@ app.get('/', function (req, res) {
         {title : 'Home'}
     );
 });
+bs.init({
+    server: true
+});
+
+// Now call methods on bs instead of the
+// main browserSync module export
+bs.reload("*.css");
+bs.reload("*.html");
 
 app.listen(3000);
